862a1ee5da820eb646e78371934ce994442cca50
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-09-09 20:55:12 +1000

    Github Discovery initial commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,103 @@
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonSearchbar,
+  IonSpinner,
+  IonRow,
+  IonCol,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonButton,
+  IonGrid,
+  IonItem
+} from '@ionic/react';
 
 const Home: React.FunctionComponent = () => {
+
+  const [loading, setLoading] = useState(false)
+  const [results, setResults] = useState([])
+
+  const fetchRepos = async (q: string) => {
+
+    if (q.trim() === "") {
+      setResults([])
+      return false;
+    }
+
+    setLoading(true)
+
+    try {
+      const data = await fetch(`https://api.github.com/search/repositories?q=${q}&sort=stars`);
+      const json = await data.json();
+      setResults(json.items || [])
+    }
+    catch(e) {
+      console.error(e)
+    }
+    finally {
+      setLoading(false)
+    }
+  }
+
+  const renderCardItems = () => {
+    return results.map((item: any) => (
+      <IonCard key={item.id} class="ion-no-margin ion-margin-vertical">
+        <IonCardHeader>
+          <IonCardTitle>{item.name}</IonCardTitle>
+        </IonCardHeader>
+        <IonCardContent>
+          {item.description}
+
+          <IonGrid class="ion-no-padding ion-margin-top">
+            <IonRow>
+              <IonCol class="ion-no-padding">
+                <a href={item.html_url} target="_blank">View on Github</a>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonCardContent>
+      </IonCard>
+    ))
+  }
+
   return (
     <>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
+          <IonTitle>Github Discovery</IonTitle>
         </IonToolbar>
+
+        <IonSearchbar
+          debounce={1000}
+          onIonInput={(e: any) => {
+            fetchRepos(e.target.value || "")
+          }}
+        />
       </IonHeader>
       <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
+        {
+          loading ? (
+            <IonRow justify-content-center>
+              <IonCol style={{
+                  display: "flex",
+                  justifyContent: "center"
+              }}>
+                <IonSpinner name="crescent" />
+              </IonCol>
+            </IonRow>
+          ) : (
+            results.length === 0 ? (
+              <h1>No content found</h1>
+            ) : (
+              renderCardItems()
+            )
+          )
+        }
       </IonContent>
     </>
   );
